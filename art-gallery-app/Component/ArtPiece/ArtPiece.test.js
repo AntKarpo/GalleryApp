7409dd7b26ecd2477a3d7b94244031a2a8c7f165
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-09-29 16:00:25 +0200

    init

--- art-gallery-app/Component/ArtPiece/ArtPiece.test.js
+++ art-gallery-app/Component/ArtPiece/ArtPiece.test.js
@@ -1,2 +1,34 @@
 import { render, screen } from "@testing-library/react";
-import ArtPieces from ".";
+import ArtPieces from "./ArtPieces";
+
+const exampleData = [
+    {
+      slug: "orange-red-and-green",
+      artist: "Steve Johnson",
+      name: "Orange Red and Green Abstract Painting",
+      imageSource:
+        "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg",
+      year: "2018",
+      genre: "Abstract Painting",
+      colors: ["#0f5855", "#e6ba15", "#b42011", "#cec4c6", "#d5561f"],
+      dimensions: { height: 2432, width: 1920, type: "jpg" },
+    },
+    {
+      slug: "blue-and-red",
+      artist: "Jung-Hua Lui",
+      name: "Blue and Red",
+      imageSource: "https://example-apis.vercel.app/assets/art/blue-and-red.jpg",
+      year: "2019",
+      genre: "Abstract Painting",
+      colors: ["#3f9eab", "#dfa597", "#323f6a", "#88d9ce", "#5a614c"],
+      dimensions: { height: 2560, width: 1920, type: "jpg" },
+    }]
+
+
+test("renders art pieces displayed as a list", () => {
+  render(<ArtPieces pieces={exampleData} />);
+
+  const artPieces = screen.getAllByRole("listitem");
+
+  expect(artPieces).toHaveLength(2);
+});
---
+++ art-gallery-app/Component/ArtPiecePreview/ArtPiecePreview.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import ArtPiecePreview from "./ArtPiecePreview";
+
+test("render img, title, artist for the pieces", () => {
+  render(
+    <ArtPiecePreview
+      artist="Steve Johnson"
+      image="https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg"
+      title="Orange Red and Green Abstract Painting"
+      slug="orange-red-and-green"
+    />
+  );
+
+  const image = screen.getByLabelText("Steve Johnson: Orange Red and Green Abstract Painting");
+  const artist = screen.getByText("Steve Johnson");
+  const title = screen.getByText("Orange Red and Green Abstract Painting");
+
+  expect(image).toHaveAttribute(
+    "src",
+    expect.stringContaining(
+      encodeURIComponent(
+        "https://example-apis.vercel.app/assets/art/orange-red-and-green.jpg"
+      )))
+
+  expect(artist).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+});
