88e465066c9823816ecacc91b66b5bffa5276067
Anton Karpowitsch <karpowitschanton@gmail.com>
2023-09-29 16:07:03 +0200

    nothign

--- art-gallery-app/pages/index.js
+++ art-gallery-app/pages/index.js
@@ -3,14 +3,8 @@ import useSWR from "swr";
 import ArtPieces from "@/Component/ArtPiece/ArtPieces";
 import Spotlight from "@/Component/Spotlight/Spotlight";
 
-export default function SpotlightPage() {
-  const { data: artPieces, error } = useSWR(
-    "https://example-apis.vercel.app/api/art"
-  );
-
-  if (error) return <div>Error loading art pieces</div>;
-  if (!artPieces) return <div>Loading...</div>;
-
+export default function SpotlightPage({artPieces}) {
+ 
   const spotlightArt = artPieces[Math.floor(Math.random() * artPieces.length)];
 
   return (
