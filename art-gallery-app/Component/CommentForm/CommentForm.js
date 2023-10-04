f48adcf2b49292be89741bea39117561a7f2ebe2
priyanka gupta <pynk1992@gmail.com>
2023-10-04 11:24:20 +0200

    css styling done

--- art-gallery-app/Component/CommentForm/CommentForm.js
+++ art-gallery-app/Component/CommentForm/CommentForm.js
@@ -1,3 +1,4 @@
+import styles from "./CommentForm.module.css";
 import React, { useState } from "react";
 
 export default function CommentForm({ onSubmitComment }) {
@@ -7,7 +8,7 @@ export default function CommentForm({ onSubmitComment }) {
     event.preventDefault();
 
     if (commentText.trim() === "") {
-      return; 
+      return;
     }
 
     const currentDate = new Date();
@@ -17,13 +18,12 @@ export default function CommentForm({ onSubmitComment }) {
     const hours = currentDate.getHours().toString();
     const minutes = currentDate.getMinutes().toString();
     const seconds = currentDate.getSeconds().toString();
-    
+
     const formattedDate = ` | ${hours}:${minutes}:${seconds} / ${day}.${month}.${year}`;
     const comment = {
       text: commentText,
       timestamp: formattedDate,
     };
-
 
     onSubmitComment(comment);
     setCommentText("");
@@ -31,16 +31,23 @@ export default function CommentForm({ onSubmitComment }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="text">Add Comment:</label>
+      <label htmlFor="text">
+        <strong>Add Comment:</strong>
+      </label>
+      <br />
       <textarea
         id="text"
         name="text"
         rows={5}
-        placeholder="Leave your comment here"
+        placeholder="Leave your comment here......"
         value={commentText}
         onChange={(e) => setCommentText(e.target.value)}
+        className={styles["textarea-input"]}
       ></textarea>
-      <button type="submit">Send Comment</button>
+      <br />
+      <button type="submit" className={styles["submit-btn"]}>
+        Send Comment
+      </button>
     </form>
   );
 }
